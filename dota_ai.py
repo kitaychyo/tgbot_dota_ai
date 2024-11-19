4fff36b52dbcbf508ca3c2fa7e8faadc12006e4b
kitaychyo <ooo85861@gmail.com>
2024-11-19 17:16:31 +0300

    Add files via upload

--- dota_ai.py
+++ dota_ai.py
@@ -12,7 +12,8 @@ class dota_ai():
     def __init__(self):
         table = pq.read_table('dota2_matches.parquet')
         self.df = table.to_pandas()
-
+        print(self.df['dire_team_id'].head(1))
+        print(self.df['winner_id'].head(1))
         self.df_player_name = pd.DataFrame()
         self.df_player_id = pd.DataFrame()
         self.df_hero = pd.DataFrame()
@@ -81,10 +82,10 @@ class dota_ai():
         X = df.drop(['winner_id'], axis=1)  # Все столбцы, кроме winner_id
         y = df['winner_id']  # Целевая переменная
 
+        y = (X['radiant_team_id'] == df['winner_id']).astype(int) + 1
+
         # Разделение данных на обучающую и тестовую выборки
         X_train, self.X_test, y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        self.X_test = df.drop(['winner_id'], axis=1)
-        self.y_test = df['winner_id']
 
         # Обучение модели
         model = RandomForestClassifier(random_state=42)
@@ -98,27 +99,70 @@ class dota_ai():
     def load_model(self):
         self.s = load('D:/model/model_fit.joblib')
 
-    # Тестовая хуета хз надо ли оно будет потом
+    # Результат
     def result(self, data):
         X = self.search_id(data)
         X = pd.DataFrame([X])
         X = X.drop(['winner_id'], axis=1)
         pred = self.s.predict(X)
-        return pred
+        if pred == self.radiant_team[1]:
+            return pred
+        else: return pred
+
+    def accuracy_test_data(self):
+        y_pred = self.s.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, y_pred)
+        return accuracy
 
     def search_id(self, data):
+
         mas_team = [self.df_team[0].to_list()] + [self.df_team_id[0].to_list()]
         mas_players = [self.df_player_name[0].to_list()] + [self.df_player_id[0].to_list()]
         mas_hero = [self.df_hero[0].to_list()] + [self.df_hero_id[0].to_list()]
 
         test = self.create_slovar(self.df.columns.to_list())
-        test['radiant_team_id'] = mas_team[1][mas_team[0].index(data['radiant_team_name'])]
-        test['dire_team_id'] = mas_team[1][mas_team[0].index(data['dire_team_name'])]
-        for i in range(1,6):
-            test[f'radiant_player_{i}_id'] = mas_players[1][mas_players[0].index(data[f'radiant_player_{i}_name'])]
-            test[f'radiant_player_{i}_hero_id'] = mas_hero[1][mas_hero[0].index(data[f'radiant_player_{i}_hero'])]
-            test[f'dire_player_{i}_id'] = mas_players[1][mas_players[0].index(data[f'dire_player_{i}_name'])]
-            test[f'dire_player_{i}_hero_id'] = mas_hero[1][mas_hero[0].index(data[f'dire_player_{i}_hero'])]
+
+        # Проверка на NaN для радиант команды
+        if pd.notna(data.get('radiant_team_name')) and data['radiant_team_name'] in mas_team[0]:
+            test['radiant_team_id'] = mas_team[1][mas_team[0].index(data['radiant_team_name'])]
+        else:
+            test['radiant_team_id'] = 1  # или любое другое значение по умолчанию
+
+        # Проверка на NaN для дир команды
+        if pd.notna(data.get('dire_team_name')) and data['dire_team_name'] in mas_team[0]:
+            test['dire_team_id'] = mas_team[1][mas_team[0].index(data['dire_team_name'])]
+        else:
+            test['dire_team_id'] = 2  # или любое другое значение по умолчанию
+
+        self.dire_team = [data.get('dire_team_name', 'Unknown'), test.get('dire_team_id', None)]
+        self.radiant_team = [data.get('radiant_team_name', 'Unknown'), test.get('radiant_team_id', None)]
+
+        for i in range(1, 6):
+            # Проверка на NaN для радиант игроков
+            radiant_player_name = data.get(f'radiant_player_{i}_name')
+            radiant_player_hero = data.get(f'radiant_player_{i}_hero')
+            if pd.notna(radiant_player_name) and radiant_player_name in mas_players[0]:
+                test[f'radiant_player_{i}_id'] = mas_players[1][mas_players[0].index(radiant_player_name)]
+            else:
+                test[f'radiant_player_{i}_id'] = None  # или любое другое значение по умолчанию
+
+            if pd.notna(radiant_player_hero) and radiant_player_hero in mas_hero[0]:
+                test[f'radiant_player_{i}_hero_id'] = mas_hero[1][mas_hero[0].index(radiant_player_hero)]
+            else:
+                test[f'radiant_player_{i}_hero_id'] = None  # или любое другое значение по умолчанию
+
+            # Проверка на NaN для дир игроков
+            dire_player_name = data.get(f'dire_player_{i}_name')
+            dire_player_hero = data.get(f'dire_player_{i}_hero')
+            if pd.notna(dire_player_name) and dire_player_name in mas_players[0]:
+                test[f'dire_player_{i}_id'] = mas_players[1][mas_players[0].index(dire_player_name)]
+            else:
+                test[f'dire_player_{i}_id'] = None  # или любое другое значение по умолчанию
+
+            if pd.notna(dire_player_hero) and dire_player_hero in mas_hero[0]:
+                test[f'dire_player_{i}_hero_id'] = mas_hero[1][mas_hero[0].index(dire_player_hero)]
+            else:
+                test[f'dire_player_{i}_hero_id'] = None
 
         return test
     def create_slovar(self,headers):
